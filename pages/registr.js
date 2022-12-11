57f64d7201fc1dfe1d53a74475323dbbc06c1c55
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2022-12-11 16:40:42 +0100

    Git initialize

---
+++ pages/registr.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Registr() {
+  return (
+    <div>
+      <Head>
+        <title>Registr</title>
+      </Head>
+      <h1>Registr</h1>
+    </div>
+  );
+}
