9b47abd3da669a9bbd85079ac689089c81f5e21e
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2022-12-13 00:38:40 +0100

    fix

--- components/employee/fetchEmployee.js
+++ components/employee/fetchEmployee.js
@@ -1,9 +1,7 @@
 export const getEmployee = async () => {
   console.log(process.env.APP_URL, "APP_URL");
   console.log(process.env.VERCEL_URL, "NEXT_PUBLIC_VERCEL_URL");
-  const response = await fetch(
-    `${process.env.VERCEL_URL || process.env.APP_URL}/api/employee`
-  );
+  const response = await fetch(`/api/employee`);
   const json = response.json();
   return json;
 };
