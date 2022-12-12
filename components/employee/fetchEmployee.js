14ecea1fba01c0b1e4e59bab00a91c129c43b082
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2022-12-12 23:57:20 +0100

    clg APP_URL

--- components/employee/fetchEmployee.js
+++ components/employee/fetchEmployee.js
@@ -1,4 +1,5 @@
 export const getEmployee = async () => {
+  console.log(process.env.APP_URL, "APP_URL");
   const response = await fetch(`${process.env.APP_URL}/api/employee`);
   const json = response.json();
   return json;
