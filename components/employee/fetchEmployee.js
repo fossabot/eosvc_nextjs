1ffb25a97694c28d5cc37c6d0302297f9a8358ed
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2022-12-13 00:10:49 +0100

    a

--- components/employee/fetchEmployee.js
+++ components/employee/fetchEmployee.js
@@ -1,8 +1,6 @@
 export const getEmployee = async () => {
   console.log(process.env.APP_URL, "APP_URL");
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_VERCEL_URL || process.env.APP_URL}/api/employee`
-  );
+  const response = await fetch(`${process.env.APP_URL}/api/employee`);
   const json = response.json();
   return json;
 };
