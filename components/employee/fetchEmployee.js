ebe82a22b719879b8a32db5ce2127baecebde63d
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2022-12-13 00:06:32 +0100

    bugfix fetch API

--- components/employee/fetchEmployee.js
+++ components/employee/fetchEmployee.js
@@ -1,6 +1,8 @@
 export const getEmployee = async () => {
   console.log(process.env.APP_URL, "APP_URL");
-  const response = await fetch(`${process.env.APP_URL}/api/employee`);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_VERCEL_URL || process.env.APP_URL}/api/employee`
+  );
   const json = response.json();
   return json;
 };
