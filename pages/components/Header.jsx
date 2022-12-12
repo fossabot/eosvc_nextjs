49bca02a64c35f4bd0e98a814987dc3aac3bf994
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2022-12-12 09:14:48 +0100

    Header fix

--- pages/components/Header.jsx
+++ pages/components/Header.jsx
@@ -3,31 +3,36 @@ import React from "react";
 
 function Header() {
   const { data: session } = useSession();
+  console.log(session, "Session");
   return (
     <header className="flex flex-row bg-blue-400 h-22 w-full p-5">
       <div className="mx-auto">1</div>
       <div className="flex items-center  ">
-        <div className="flex flex-col items-end text-sm font-bold text-white p-2">
-          <p>{session.user.name}</p>
-          <p>{session.user.email}</p>
-        </div>
-        <div>
-          {" "}
-          {session.user.image ? (
-            <img
-              className="rounded-full w-16 h-16"
-              src={session.user.image}
-              alt="User img"
-              referrerPolicy="no-referrer"
-            />
-          ) : (
-            <img
-              className="rounded-full w-8 h-8"
-              src={"/assets/nouser.png"}
-              alt="User img"
-            />
-          )}
-        </div>
+        {session && (
+          <>
+            <div className="flex flex-col items-end text-sm font-bold text-white p-2">
+              <p>{session.user.name}</p>
+              <p>{session.user.email}</p>
+            </div>
+            <div>
+              {" "}
+              {session.user.image ? (
+                <img
+                  className="rounded-full w-16 h-16"
+                  src={session.user.image}
+                  alt="User img"
+                  referrerPolicy="no-referrer"
+                />
+              ) : (
+                <img
+                  className="rounded-full w-8 h-8"
+                  src={"/assets/nouser.png"}
+                  alt="User img"
+                />
+              )}
+            </div>
+          </>
+        )}
       </div>
     </header>
   );
