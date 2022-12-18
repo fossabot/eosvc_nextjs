3fddcb5862fc5a69bb1e92f9cc77a07ced442b36
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2022-12-18 20:55:46 +0100

    bugfix

--- database/controllers/user/getUserId.js
+++ database/controllers/user/getUserId.js
@@ -6,10 +6,8 @@ export async function getUserId(req, res) {
     console.log(userEmail);
     if (userEmail) {
       const user = await Users.findOne({ email: userEmail });
-      await res.status(200).json(user);
-      await res.end();
+      res.status(200).json(user);
     }
-
     res.status(404).json({ error: "User not selected" });
   } catch (error) {
     res.status(404).json({ error: "Error While Fetching User" });
