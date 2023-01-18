e71cd585f0ae0d52c5ffd86e605438dcacb6f9aa
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2023-01-18 13:32:49 +0100

    bugfix - react-query (old package)

--- modules/user/ShowProfile.jsx
+++ modules/user/ShowProfile.jsx
@@ -1,4 +1,4 @@
-import { useMutation } from "react-query";
+import { useMutation } from "@tanstack/react-query";
 import { useDispatch } from "react-redux";
 import { updateUser } from "./updateUser";
 import { updateUserPass } from "./updateUserPass";
